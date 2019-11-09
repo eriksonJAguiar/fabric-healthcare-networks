2767c61bece377356d0d4fcf0decb5e8e1d8e0f3
eriksonJAguiar <erjulioaguiar@gmail.com>
2019-11-09 19:03:26 -0300

    configuracao da rede

--- hyperledger-BCShield/network/chaincode_config.sh
+++ hyperledger-BCShield/network/chaincode_config.sh
@@ -6,25 +6,25 @@ export CORE_PEER_ADDRESS=peer0.hprovider.healthcare.com:7051
 
 export CORE_PEER_LOCALMSPID=HProviderMSP
 
-peer chaincode install -n Healthcare-records -v 1.0 -l golang -p /opt/gopath/src/github.com/chaincode/Healthcare-records
+peer chaincode install -n Healthcare-records -v 1.0 -l golang -p github.com/chaincode/Healthcare-records
 
 export CORE_PEER_ADDRESS=peer1.hprovider.healthcare.com:7051
 
-peer chaincode install -n Healthcare-records -v 1.0 -l go -p /opt/gopath/src/github.com/chaincode/Healthcare-records
+peer chaincode install -n Healthcare-records -v 1.0 -l golang -p github.com/chaincode/Healthcare-records
 
-export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto-config/peerOrganizations/reseach.healthcare.com/users/Admin\@reseach.healthcare.com/msp/
+export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto-config/peerOrganizations/research.healthcare.com/users/Admin\@research.healthcare.com/msp/
 
-export CORE_PEER_ADDRESS=peer0.reseach.healthcare.com:7051
+export CORE_PEER_ADDRESS=peer0.research.healthcare.com:7051
 
-export CORE_PEER_LOCALMSPID=ReseachMSP
+export CORE_PEER_LOCALMSPID=ResearchMSP
 
-peer chaincode install -n Healthcare-records -v 1.0 -l go -p /opt/gopath/src/github.com/chaincode/Healthcare-records
+peer chaincode install -n Healthcare-records -v 1.0 -l golang -p github.com/chaincode/Healthcare-records
 
-export CORE_PEER_ADDRESS=peer1.reseach.healthcare.com:7051
+export CORE_PEER_ADDRESS=peer1.research.healthcare.com:7051
 
-peer chaincode install -n Healthcare-records -v 1.0 -l go -p /opt/gopath/src/github.com/chaincode/Healthcare-records
+peer chaincode install -n Healthcare-records -v 1.0 -l golang -p github.com/chaincode/Healthcare-records
 
-peer chaincode instantiate -o ordererhp.healthcare.com:7050 -C healthchannel -l go -n Healthcare-records -v 1.0 -c '{"Args":[]}'
+peer chaincode instantiate -o ordererhp.healthcare.com:7050 -C healthchannel -l golang -n Healthcare-records -v 1.0 -c '{"Args":[]}'
 
 export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto-config/peerOrganizations/hprovider.healthcare.com/users/Admin\@hprovider.healthcare.com/msp/
 
@@ -32,6 +32,6 @@ export CORE_PEER_ADDRESS=peer0.hprovider.healthcare.com:7051
 
 export CORE_PEER_LOCALMSPID=HProviderMSP
 
-peer chaincode invoke -n Healthcare-records -c '{"Args":["123", "Product one", "10"], "Function":"registerProduct"}' -C healthchannel
+peer chaincode invoke -n Healthcare-records -c '{"Args":["123", "222", "Running", "100", "-1828192 121212"], "Function":"initRecord"}' -C healthchannel
 
-peer chaincode query -n Healthcare-records -c '{"Args":["123"], "Function":"getProduct"}' -C healthchannel
+peer chaincode query -n Healthcare-records -c '{"Args":["123"], "Function":"readRecord"}' -C healthchannel
