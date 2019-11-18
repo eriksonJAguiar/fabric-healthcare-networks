04308a6c39dd2f4a20fe0e5a1a0b4bceec5fcbad
EriksonJAguiar <erjulioaguiar@gmail.com>
2019-11-18 18:42:58 -0300

    script for start peers and chaincode

--- hyperledger-BCShield/network/chaincode_config.sh
+++ hyperledger-BCShield/network/chaincode_config.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 
-export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto-config/peerOrganizations/hprovider.healthcare.com/users/Admin\@hprovider.healthcare.com/msp/
+docker exec -it -e CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto-config/peerOrganizations/hprovider.healthcare.com/users/Admin\@hprovider.healthcare.com/msp/ cli
 
-export CORE_PEER_ADDRESS=peer0.hprovider.healthcare.com:7051
+docker exec -it -e  CORE_PEER_ADDRESS=peer0.hprovider.healthcare.com:7051 cli
 
-export CORE_PEER_LOCALMSPID=HProviderMSP
+docker exec -it -e  CORE_PEER_LOCALMSPID=HProviderMSP cli
 
-peer chaincode install -n Healthcare-records -v 1.0 -l golang -p github.com/chaincode/Healthcare-records
+docker exec -it cli peer chaincode install -n Health_DICOM -v 1.0 -l golang -p github.com/chaincode/Health_DICOM
 
-export CORE_PEER_ADDRESS=peer1.hprovider.healthcare.com:7051
+docker exec -it -e CORE_PEER_ADDRESS=peer1.hprovider.healthcare.com:7051 cli
 
-peer chaincode install -n Healthcare-records -v 1.0 -l golang -p github.com/chaincode/Healthcare-records
+docker exec -it cli peer chaincode install -n Health_DICOM -v 1.0 -l golang -p github.com/chaincode/Health_DICOM
 
-export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto-config/peerOrganizations/research.healthcare.com/users/Admin\@research.healthcare.com/msp/
+docker exec -it -e CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto-config/peerOrganizations/research.healthcare.com/users/Admin\@research.healthcare.com/msp/ cli
 
-export CORE_PEER_ADDRESS=peer0.research.healthcare.com:7051
+docker exec -it -e CORE_PEER_ADDRESS=peer0.research.healthcare.com:7051 cli
 
-export CORE_PEER_LOCALMSPID=ResearchMSP
+docker exec -it -e CORE_PEER_LOCALMSPID=ResearchMSP cli
 
-peer chaincode install -n Healthcare-records -v 1.0 -l golang -p github.com/chaincode/Healthcare-records
+docker exec -it cli peer chaincode install -n Health_DICOM -v 1.0 -l golang -p github.com/chaincode/Health_DICOM
 
-export CORE_PEER_ADDRESS=peer1.research.healthcare.com:7051
+docker exec -it -e CORE_PEER_ADDRESS=peer1.research.healthcare.com:7051 cli
 
-peer chaincode install -n Healthcare-records -v 1.0 -l golang -p github.com/chaincode/Healthcare-records
+docker exec -it cli peer chaincode install -n Health_DICOM -v 1.0 -l golang -p github.com/chaincode/Health_DICOM
 
-peer chaincode instantiate -o ordererhp.healthcare.com:7050 -C healthchannel -l golang -n Healthcare-records -v 1.0 -c '{"Args":[]}'
+docker exec -it cli peer chaincode instantiate -o ordererhp.healthcare.com:7050 -C healthchannel -l golang -n Health_DICOM -v 1.0 -c '{"Args":[]}'
 
-export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto-config/peerOrganizations/hprovider.healthcare.com/users/Admin\@hprovider.healthcare.com/msp/
+docker exec -it -e CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto-config/peerOrganizations/hprovider.healthcare.com/users/Admin\@hprovider.healthcare.com/msp/ cli
 
-export CORE_PEER_ADDRESS=peer0.hprovider.healthcare.com:7051
+docker exec -it -e CORE_PEER_ADDRESS=peer0.hprovider.healthcare.com:7051 cli
 
-export CORE_PEER_LOCALMSPID=HProviderMSP
+docker exec -it -e CORE_PEER_LOCALMSPID=HProviderMSP cli
 
-peer chaincode invoke -n Healthcare-records -c '{"Args":["123", "222", "Running", "100", "-1828192 121212"], "Function":"initRecord"}' -C healthchannel
+peer chaincode invoke -n Health_DICOM -c '{"Args":["123", "222", "Running", "100", "-1828192 121212"], "Function":"initRecord"}' -C healthchannel
 
-peer chaincode query -n Healthcare-records -c '{"Args":["123"], "Function":"readRecord"}' -C healthchannel
+peer chaincode query -n Health_DICOM -c '{"Args":["123"], "Function":"readRecord"}' -C healthchannel
--- hyperledger-BCShield/network/start.sh
+++ hyperledger-BCShield/network/start.sh
@@ -5,40 +5,38 @@ CHANNEL_NAME=healthchannel
 docker-compose -f docker-compose.yaml down
 docker-compose -f docker-compose.yaml up -d
 
-docker exec -it cli bash
+docker exec -it cli peer channel create -o ordererhp.healthcare.com:7050 -c healthchannel -f ./channel-artifacts/channel.tx
 
-peer channel create -o ordererhp.healthcare.com:7050 -c healthchannel -f ./channel-artifacts/channel.tx
+docker exec -it cli peer channel join -b healthchannel.block
 
-peer channel join -b healthchannel.block
+docker exec -it -e CORE_PEER_ADDRESS=peer1.hprovider.healthcare.com:7051 cli
 
-export CORE_PEER_ADDRESS=peer1.hprovider.healthcare.com:7051
+docker exec -it cli peer channel join -b healthchannel.block
 
-peer channel join -b healthchannel.block
+docker exec -it -e CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto-config/peerOrganizations/research.healthcare.com/users/Admin\@research.healthcare.com/msp/ cli
 
-export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto-config/peerOrganizations/research.healthcare.com/users/Admin\@research.healthcare.com/msp/
+docker exec -it -e CORE_PEER_ADDRESS=peer0.research.healthcare.com:7051 cli
 
-export CORE_PEER_ADDRESS=peer0.research.healthcare.com:7051
+docker exec -it -e CORE_PEER_LOCALMSPID=ResearchMSP cli
 
-export CORE_PEER_LOCALMSPID=ResearchMSP
+docker exec -it cli peer channel join -b healthchannel.block
 
-peer channel join -b healthchannel.block
+docker exec -it -e CORE_PEER_ADDRESS=peer1.research.healthcare.com:7051 cli
 
-export CORE_PEER_ADDRESS=peer1.research.healthcare.com:7051
+docker exec -it cli peer channel join -b healthchannel.block
 
-peer channel join -b healthchannel.block
+docker exec -it -e CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto-config/peerOrganizations/hprovider.healthcare.com/users/Admin\@hprovider.healthcare.com/msp/ cli
 
-export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto-config/peerOrganizations/hprovider.healthcare.com/users/Admin\@hprovider.healthcare.com/msp/
+docker exec -it -e CORE_PEER_ADDRESS=peer0.hprovider.healthcare.com:7051 cli
 
-export CORE_PEER_ADDRESS=peer0.hprovider.healthcare.com:7051
+docker exec -it -e CORE_PEER_LOCALMSPID=HProviderMSP cli
 
-export CORE_PEER_LOCALMSPID=HProviderMSP
+docker exec -it cli peer channel update -o ordererhp.healthcare.com:7050 -c healthchannel -f ./channel-artifacts/${CORE_PEER_LOCALMSPID}-anchors.tx
 
-peer channel update -o ordererhp.healthcare.com:7050 -c healthchannel -f ./channel-artifacts/${CORE_PEER_LOCALMSPID}-anchors.tx
+docker exec -it -e CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto-config/peerOrganizations/research.healthcare.com/users/Admin\@research.healthcare.com/msp/ cli
 
-export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto-config/peerOrganizations/research.healthcare.com/users/Admin\@research.healthcare.com/msp/
+docker exec -it -e CORE_PEER_ADDRESS=peer0.research.healthcare.com:7051 cli
 
-export CORE_PEER_ADDRESS=peer0.research.healthcare.com:7051
+docker exec -it -e CORE_PEER_LOCALMSPID=ResearchMSP cli
 
-export CORE_PEER_LOCALMSPID=ResearchMSP
-
-peer channel update -o ordererhp.healthcare.com:7050 -c healthchannel -f ./channel-artifacts/${CORE_PEER_LOCALMSPID}-anchors.tx
+docker exec -it cli peer channel update -o ordererhp.healthcare.com:7050 -c healthchannel -f ./channel-artifacts/${CORE_PEER_LOCALMSPID}-anchors.tx
