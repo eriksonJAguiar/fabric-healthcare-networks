1d8187e68726d75897b0b3b48eb1b1fc8272c621
eriksonJAguiar <erjulioaguiar@gmail.com>
2019-11-30 18:10:38 -0300

    fix chaicode path

--- network-health/clean_up.sh
+++ network-health/clean_up.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-# orderer
-docker-compose -f docker-compose-orderer.yml down
-# node1
-docker-compose -f docker-compose-node1.yml down
-docker rm $(docker ps -aq)
-docker rmi $(docker images net-* -q)
-# node2
-docker-compose -f docker-compose-node2.yml down
-docker rm $(docker ps -aq)
-docker rmi $(docker images net-* -q)
-# node3
-docker-compose -f docker-compose-node3.yml down
-docker rm $(docker ps -aq)
-docker rmi $(docker images net-* -q)
+docker rm -f $(docker ps -a -q)
+
+docker volume -f prune
+
+docker network -f prune
+
