ed55dc1b820f725e8ec1abcce6a03c4cbec2a87f
Erikson Aguiar <erjulioaguiar@Eriksons-iMac.local>
2020-01-17 13:40:07 -0200

    modify network

---
+++ network-health/chaincode/HRecords-contract/index.js
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const healthcareLabsContract = require('./lib/healthcare-labs-contract');
+module.exports.contracts = [ healthcareLabsContract ];
+
+//module.exports.HealthcareLogsContract = HealthcareLogsContract;
---
+++ network-health/start-full.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export RESEARCH_HOSTNAME=$(hostname)
+export HPROVIDER_HOSTNAME=$(hostname)
+export ORDERER_HOSTNAME=$(hostname)
+
+
+#docker network create -d overlay --attachable healthNetwork
+
+docker-compose -f ./compose-files/docker-compose-orderer.yml up -d
+
+docker-compose -f ./compose-files/docker-compose-netfull.yml up -d
