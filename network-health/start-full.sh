1cb3a95e798f7876d753485d628f97cd285f00e2
eriksonJAguiar <erjulioaguiar@gmail.com>
2020-01-17 13:48:31 -0300

    alteracao chaicode

--- network-health/start-full.sh
+++ network-health/start-full.sh
@@ -7,6 +7,8 @@ export ORDERER_HOSTNAME=$(hostname)
 
 #docker network create -d overlay --attachable healthNetwork
 
+./clean_up.sh
+
 docker-compose -f ./compose-files/docker-compose-orderer.yml up -d
 
-docker-compose -f ./compose-files/docker-compose-netfull.yml up -d
+docker-compose -f ./compose-files/docker-compose-cli.yml up -d
