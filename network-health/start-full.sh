6a15b123178da148d8925fb2fe79e560b18e9a11
eriksonJAguiar <erjulioaguiar@gmail.com>
2020-01-17 15:01:36 -0200

    new file docker

--- network-health/start-full.sh
+++ network-health/start-full.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-export RESEARCH_HOSTNAME=$(hostname)
-export HPROVIDER_HOSTNAME=$(hostname)
-export ORDERER_HOSTNAME=$(hostname)
-
-
-#docker network create -d overlay --attachable healthNetwork
+export IMAGE_TAG=1.4
 
 ./clean_up.sh
 
-docker-compose -f ./compose-files/docker-compose-orderer.yml up -d
+docker-compose up -d
 
-docker-compose -f ./compose-files/docker-compose-cli.yml up -d
+
