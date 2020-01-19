c9820b5ce154424f2420699558e9be3cf1323ce3
eriksonJAguiar <erjulioaguiar@gmail.com>
2020-01-19 17:31:54 -0200

    enrollAdmin

--- hyperledger-BCShield/chaincode/HRecords-contract/api/enrollAdmin.js
+++ hyperledger-BCShield/chaincode/HRecords-contract/api/enrollAdmin.js
@@ -36,7 +36,7 @@ async function main() {
                 certificate: enrollment.certificate,
                 privateKey: enrollment.key.toBytes(),
             },
-            mspId: 'Org1MSP',
+            mspId: 'HProviderMSP',
             type: 'X.509',
         };
         await wallet.put('admin', x509Identity);
