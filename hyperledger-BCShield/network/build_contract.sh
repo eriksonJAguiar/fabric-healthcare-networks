e6285d562051bbdc4d2540c53de3a1cf8aeb125d
eriksonJAguiar <erjulioaguiar@gmail.com>
2020-01-22 13:14:59 -0200

    fix channel

--- hyperledger-BCShield/network/build_contract.sh
+++ hyperledger-BCShield/network/build_contract.sh
@@ -20,25 +20,27 @@ PEER0_HPROVIDER="docker exec
 cli
 bash"
 
-${PEER0_HPROVIDER} \
+echo "Create channel ..."
+
+${PEER0_HPROVIDER}
 docker exec -it cli peer channel create -o ordererhp.healthcare.com:7050 -c ${CHANNEL_NAME} -f ./channel-artifacts/channel.tx
 
 #----------- Create Channel -------------
 
-echo "create channel for peer0.hprovider ..."
+echo "join channel for peer0.hprovider ..."
 
 
 docker exec  -e CORE_PEER_LOCALMSPID=HProviderMSP -e CORE_PEER_ADDRESS=peer0.hprovider.healthcare.com:7051 -e CORE_PEER_MSPCONFIGPATH=${HPROVIDER_MSPCONFIGPATH} -e CORE_PEER_TLS_ROOTCERT_FILE=${HPROVIDER_TLS_ROOTCERT_FILE} cli peer channel join -b ${CHANNEL_NAME}.block
 #docker exec -it cli peer channel join -b ${CHANNEL_NAME}.block
 
 
-echo "create channel for peer1.hprovider ..."
+echo "join channel for peer1.hprovider ..."
 
 
 docker exec  -e CORE_PEER_LOCALMSPID=HProviderMSP -e CORE_PEER_ADDRESS=peer1.hprovider.healthcare.com:7051 -e CORE_PEER_MSPCONFIGPATH=${HPROVIDER_MSPCONFIGPATH} -e CORE_PEER_TLS_ROOTCERT_FILE=${HPROVIDER_TLS_ROOTCERT_FILE} cli peer channel join -b ${CHANNEL_NAME}.block
 #docker exec -it cli peer channel join -b ${CHANNEL_NAME}.block 
 
-echo "create channel for peer2.hprovider ..."
+echo "join channel for peer2.hprovider ..."
 
 
 docker exec  -e CORE_PEER_LOCALMSPID=HProviderMSP -e CORE_PEER_ADDRESS=peer2.hprovider.healthcare.com:7051 -e CORE_PEER_MSPCONFIGPATH=${HPROVIDER_MSPCONFIGPATH} -e CORE_PEER_TLS_ROOTCERT_FILE=${HPROVIDER_TLS_ROOTCERT_FILE} cli peer channel join -b ${CHANNEL_NAME}.block
@@ -47,7 +49,7 @@ docker exec  -e CORE_PEER_LOCALMSPID=HProviderMSP -e CORE_PEER_ADDRESS=peer2.hpr
 docker exec  -e CORE_PEER_LOCALMSPID=HProviderMSP -e CORE_PEER_ADDRESS=peer3.hprovider.healthcare.com:7051 -e CORE_PEER_MSPCONFIGPATH=${HPROVIDER_MSPCONFIGPATH} -e CORE_PEER_TLS_ROOTCERT_FILE=${HPROVIDER_TLS_ROOTCERT_FILE} cli peer channel join -b ${CHANNEL_NAME}.block
 
 
-echo "create channel for peer0.research ..."
+echo "join channel for peer0.research ..."
 
 docker exec  -e CORE_PEER_LOCALMSPID=ResearchMSP -e CORE_PEER_ADDRESS=peer0.research.healthcare.com:7051 -e CORE_PEER_MSPCONFIGPATH=${RESEARCH_MSPCONFIGPATH} -e CORE_PEER_TLS_ROOTCERT_FILE=${RESEARCH_TLS_ROOTCERT_FILE} cli peer channel join -b ${CHANNEL_NAME}.block
 #${PEER0_RESEARCH} \
@@ -55,11 +57,11 @@ docker exec  -e CORE_PEER_LOCALMSPID=ResearchMSP -e CORE_PEER_ADDRESS=peer0.rese
 #docker exec -it cli peer channel join -b ${CHANNEL_NAME}.block 
 
 
-echo "create channel for peer1.research ..."
+echo "join channel for peer1.research ..."
 
 docker exec  -e CORE_PEER_LOCALMSPID=ResearchMSP -e CORE_PEER_ADDRESS=peer1.research.healthcare.com:7051 -e CORE_PEER_MSPCONFIGPATH=${RESEARCH_MSPCONFIGPATH} -e CORE_PEER_TLS_ROOTCERT_FILE=${RESEARCH_TLS_ROOTCERT_FILE} cli peer channel join -b ${CHANNEL_NAME}.block
 #${PEER1_RESEARCH} \
-#docker exec -it cli peer channel create -o ordererhp.healthcare.com:7050 -c ${CHANNEL_NAME} -f ./channel-artifacts/channel.tx \
+#docker exec -it cli peer channel join -o ordererhp.healthcare.com:7050 -c ${CHANNEL_NAME} -f ./channel-artifacts/channel.tx \
 #docker exec -it cli peer channel join -b ${CHANNEL_NAME}.block 
 
 
