4ead6f8f503c23257b81d8ae83f62fe32a07634c
eriksonJAguiar <erjulioaguiar@gmail.com>
2020-01-25 15:36:01 -0200

    change api

--- hyperledger-BCShield/network/app-api/index.js
+++ hyperledger-BCShield/network/app-api/index.js
@@ -60,16 +60,16 @@ app.get('/api/readHealthcareLabs/:index', async function (req, res) {
         console.log(`Wallet path: ${walletPath}`);
 
         // Check to see if we've already enrolled the user.
-        const userExists = await wallet.exists('erikson2');
+        const userExists = await wallet.exists('user1');
         if (!userExists) {
-            console.log('An identity for the user "erikson2" does not exist in the wallet');
+            console.log('An identity for the user "user1" does not exist in the wallet');
             console.log('Run the registerUser.js application before retrying');
             return;
         }
 
         // Create a new gateway for connecting to our peer node.
         const gateway = new Gateway();
-        await gateway.connect(ccpPath, { wallet, identity: 'erikson2', discovery: { enabled: true, asLocalhost: true } });
+        await gateway.connect(ccpPath, { wallet, identity: 'user1', discovery: { enabled: true, asLocalhost: true } });
         console.log('Gateway criado');
 
         // Get the network (channel) our contract is deployed to.
@@ -106,16 +106,16 @@ app.post('/api/createHealthcareLabs/', async function (req, res) {
         console.log(`Wallet path: ${walletPath}`);
 
         // Check to see if we've already enrolled the user.
-        const userExists = await wallet.exists('erikson2');
+        const userExists = await wallet.exists('user1');
         if (!userExists) {
-            console.log('An identity for the user "erikson2" does not exist in the wallet');
+            console.log('An identity for the user "user1" does not exist in the wallet');
             console.log('Run the registerUser.js application before retrying');
             return;
         }
 
         // Create a new gateway for connecting to our peer node.
         const gateway = new Gateway();
-        await gateway.connect(ccpPath, { wallet, identity: 'erikson2', discovery: { enabled: true, asLocalhost: false } });
+        await gateway.connect(ccpPath, { wallet, identity: 'user1', discovery: { enabled: true, asLocalhost: false } });
 
         // Get the network (channel) our contract is deployed to.
         const network = await gateway.getNetwork('healthchannel');
@@ -154,7 +154,7 @@ app.post('/api/createHealthcareLabs/', async function (req, res) {
 //         console.log(`Wallet path: ${walletPath}`);
 
 //         // Check to see if we've already enrolled the user.
-//         const userExists = await wallet.exists('erikson2');
+//         const userExists = await wallet.exists('user1');
 //         if (!userExists) {
 //             console.log('An identity for the user "user1" does not exist in the wallet');
 //             console.log('Run the registerUser.js application before retrying');
