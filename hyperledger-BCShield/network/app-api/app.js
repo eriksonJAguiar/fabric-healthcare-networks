f0d5c766cac43e420cc522ce8906349094525057
eriksonJAguiar <erjulioaguiar@gmail.com>
2020-01-27 19:45:00 -0200

    docker command

--- hyperledger-BCShield/network/app-api/app.js
+++ hyperledger-BCShield/network/app-api/app.js
@@ -8,6 +8,7 @@ app.use(bodyParser.json());
 const { FileSystemWallet, Gateway } = require('fabric-network');
 const path = require('path');
 const ccpPath = path.resolve(__dirname, '.', 'Config', 'connection-profile.json');
+const { exec } = require('child_process');
 
 
 // app.get('/api/queryallcars', async function (req, res) {
@@ -54,41 +55,19 @@ const ccpPath = path.resolve(__dirname, '.', 'Config', 'connection-profile.json'
 app.get('/api/readHealthcareLabs/:index', async function (req, res) {
     try {
 
-        // Create a new file system based wallet for managing identities.
-        const walletPath = path.join(process.cwd(), 'wallet');
-        const wallet = new FileSystemWallet(walletPath);
-        console.log(`Wallet path: ${walletPath}`);
+        //req.params.index
 
-        // Check to see if we've already enrolled the user.
-        const userExists = await wallet.exists('user1');
-        if (!userExists) {
-            console.log('An identity for the user "user1" does not exist in the wallet');
-            console.log('Run the registerUser.js application before retrying');
-            return;
-        }
+        const peer = 'peer0.hrecords.healthcare.com';
 
-        // Create a new gateway for connecting to our peer node.
-        const gateway = new Gateway();
-        await gateway.connect(ccpPath, { wallet, identity: 'user1', discovery: { enabled: true, asLocalhost: true } });
-        console.log('Gateway criado');
-
-        // Get the network (channel) our contract is deployed to.
-        const network = await gateway.getNetwork('ch1'); 
-        console.log('canal acessado');
-
-        // Get the contract from the network.
-        const contract = network.getContract('hrecords');
-        console.log('canal acessado');
-        //const contract = network.getContract('Healthcare-Labs@0.0.4');
-        // Evaluate the specified transaction.
-        // queryCar transaction - requires 1 argument, ex: ('queryCar', 'CAR4')
-        // queryAllCars transaction - requires no arguments, ex: ('queryAllCars')
-        const result = await contract.evaluateTransaction('readHealthcareLabs', req.params.index);
-        console.log(`Transaction has been evaluated, result is: ${result.toString()}`);
-        res.status(200).json({response: result.toString()});
-
-        // Disconnect from the gateway.
-        await gateway.disconnect();
+        
+        exec(`docker exec -it cli peer chaincode query -n HRecords-contract -c '{"Args":["readHealthcareLabs","${eq.params.index}"]}' -C healthchannel`, (err, stdout, stderr) => {
+            if (err) {
+                console.error(err)
+            } else {
+            console.log(`stdout: ${stdout}`);
+            console.log(`stderr: ${stderr}`);
+            }
+        });
 
     } catch (error) {
         console.error(`Failed to evaluate transaction: ${error}`);
@@ -100,44 +79,23 @@ app.get('/api/readHealthcareLabs/:index', async function (req, res) {
 app.post('/api/createHealthcareLabs/', async function (req, res) {
     try {
 
-        // Create a new file system based wallet for managing identities.
-        const walletPath = path.join(process.cwd(), 'wallet');
-        const wallet = new FileSystemWallet(walletPath);
-        console.log(`Wallet path: ${walletPath}`);
-
-        // Check to see if we've already enrolled the user.
-        const userExists = await wallet.exists('user1');
-        if (!userExists) {
-            console.log('An identity for the user "user1" does not exist in the wallet');
-            console.log('Run the registerUser.js application before retrying');
-            return;
-        }
-
-        // Create a new gateway for connecting to our peer node.
-        const gateway = new Gateway();
-        await gateway.connect(ccpPath, { wallet, identity: 'user1', discovery: { enabled: true, asLocalhost: false } });
-
-        // Get the network (channel) our contract is deployed to.
-        const network = await gateway.getNetwork('ch1');
-
-        // Get the contract from the network.
-        const contract = network.getContract('hrecords'); 
-        //const contract = network.getContract('Healthcare-Labs@0.0.4');
-        // Submit the specified transaction.
-        // acessUser,  
-        // owner,
-        // documentId,
-        // documentType,
-        // timeAccess
-        // createCar transaction - requires 5 argument, ex: ('createCar', 'CAR12', 'Honda', 'Accord', 'Black', 'Tom')
-        // changeCarOwner transaction - requires 2 args , ex: ('changeCarOwner', 'CAR10', 'Dave')
-
-        await contract.submitTransaction('createHealthcareLabs', req.body.labsId, req.body.refDocument, req.body.refIFPS, req.body.owner, req.body.documentType, Date.now().toString());
-        console.log('Transaction has been submitted');
-        res.send('Transaction has been submitted');
+        //await contract.submitTransaction('createHealthcareLabs', req.body.labsId, req.body.refDocument, req.body.refIFPS, req.body.owner, req.body.documentType, Date.now().toString());
+        //console.log('Transaction has been submitted');
+        //res.send('Transaction has been submitted');
 
         // Disconnect from the gateway.
-        await gateway.disconnect();
+        // await gateway.disconnect();
+
+        const args = `{"Args":["createHealthcareLabs","${req.body.labsId}", "${req.body.refDocument}", "${req.body.refIFPS}", "${req.body.owner}", "${req.body.documentType}", "${Date.now().toString()}"]}`;
+
+        exec(`docker exec -it cli peer chaincode invoke -n HRecords-contract -c ${args} -C healthchannel`, (err, stdout, stderr) => {
+            if (err) {
+                console.error(err)
+            } else {
+            console.log(`stdout: ${stdout}`);
+            console.log(`stderr: ${stderr}`);
+            }
+        });
 
     } catch (error) {
         console.error(`Failed to submit transaction: ${error}`);
